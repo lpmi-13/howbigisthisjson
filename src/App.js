799fcee36113a296eafbb8b2ca6d31618c441729
Adam Leskis <leskis@gmail.com>
2023-01-07 20:05:10 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const jsonBlob = {
+    customer1: {
+        ID: 3,
+        location: "UK",
+        orders: 3,
+    },
+    customer3: {
+        ID: 8,
+        location: "UK",
+        orders: 9,
+    },
+    customer2: {
+        ID: 6,
+        location: "UK",
+        orders: 13,
+    },
+};
+
+function byteLength(str) {
+    // returns the byte length of an utf8 string
+    var s = str.length;
+    for (var i = str.length - 1; i >= 0; i--) {
+        var code = str.charCodeAt(i);
+        if (code > 0x7f && code <= 0x7ff) s++;
+        else if (code > 0x7ff && code <= 0xffff) s += 2;
+        if (code >= 0xdc00 && code <= 0xdfff) i--; //trail surrogate
+    }
+    return s;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <main>
+                <p>how big is this json?</p>
+                <div className="codeblock">
+                    <pre>{JSON.stringify(jsonBlob, null, 2)}</pre>
+                </div>
+                <div className="answer">
+                    it is {byteLength(JSON.stringify(jsonBlob))} bytes long!
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
